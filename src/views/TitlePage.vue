147b4ca1bd7ef1755e98983bd687b9a22324751c
fernandojose95 <josefernandohernandezpaz@gmail.com>
2019-06-27 20:15:56 -0600

    obtencion de datos de contribuidores

--- src/views/TitlePage.vue
+++ src/views/TitlePage.vue
@@ -27,7 +27,7 @@
     import GlobalFunction from '../mixins/globalFunctions';
 
     export default {
-        name: "Portada",
+        name: "TitlePage",
         mixins: [GlobalFunction],
         components: {
             Header,
@@ -37,7 +37,6 @@
         data() {
             return {
                 detailsSite: [],
-                urlItem: this.$domainOmeka + 'api/item_sets/',
                 urlListItem: this.$domainOmeka + 'api/items?item_set_id=',
                 urlImage: this.$domainOmeka + 'api/media/',
                 imagesArray: [],
@@ -117,109 +116,34 @@
                         });
                 })
             },
-            buildBodyPage(id_items_set) {
+            async buildBodyPage() {
 
-                let urlColaboradores='',urlVideos='';
+                let urlColaboradores = '', urlVideos = '', responseColaborares, responseVideos;
                 urlColaboradores = this.$domainOmeka + 'api/item_sets?resource_class_id=97&site_id=' + this.dataSite.id;
                 urlVideos = this.$domainOmeka + 'api/item_sets?resource_class_id=38&site_id=' + this.dataSite.id;
-                // this.$axios.all([
-                //     this.$axios.get(urlColaboradores),
-                //     this.$axios.get(urlVideos),
-                // ])
-                //     .then(this.$axios.spread(responseColaborares,responseVideos)=>{
-                //
-                // })
 
-                // for (const id_item of id_items_set) {
-                //     this.$axios(this.urlListItem + id_item)
-                //         .then((response) => {
-                //             let dataItem = response.data;
-                //             // for (const item of dataItem) {
-                //             // console.log(item);
-                //             // }
-                //             // if (dataItem['o:resource_class'] !== null) {
-                //             //     console.log(dataItem['o:resource_class']);
-                //             // }
-                //         });
-                //
-                // }
-                //recorremos los ids de los items
-                // id_items.forEach((id_item) => {
-                //
-                //     this.$axios(this.urlItem + id_item)
-                //         .then((response) => {
-                //             let dataItem = response.data;
-                //
-                //             //validamos si la propiedad 'o:resource_class' existe previamente
-                //             if (dataItem['o:resource_class'] !== null) {
-                //                 this.$axios(this.urlListItem + id_item)
-                //                     .then((response2) => {
-                //                         let counter = 0;
-                //                         console.log(response2);
-                //
-                //                         response2.data.forEach((dataResponse) => {
-                //                             // console.log(dataResponse['o:media']);
-                //                             this.$axios(dataResponse['o:media'][0]['@id'])
-                //                                 .then((response3) => {
-                //                                     // console.log(response3);
-                //                                     // 27 = InteractiveResource, 38 = AudioVisualDocument
-                //                                     if (dataItem['o:resource_class']['o:id'] === 27) {
-                //                                         let dataImage = {
-                //                                             id: dataResponse['o:media'][0]['o:id'],
-                //                                             title: this.getAttribEmptyOrFilled(dataResponse, 'dcterms:title'),
-                //                                             description: this.getAttribEmptyOrFilled(dataResponse, 'dcterms:description'),
-                //                                             media_url: this.getMediaEmptyOrFilled(response3.data),
-                //                                         };
-                //                                         this.containerImgs.push(dataImage);
-                //                                     } else if (dataItem['o:resource_class']['o:id'] === 38 && counter <= 6) {
-                //                                         let url_media = '';
-                //                                         let media_type = this.getTypeMedia(response3.data);
-                //                                         let urls_images = {
-                //                                             large: this.urlImageVideo,
-                //                                             medium: this.urlImageVideo,
-                //                                         };
-                //                                         if (response3.data['o:original_url'] !== null) {
-                //                                             url_media = response3.data['o:original_url']
-                //                                         } else {
-                //                                             let id_video = response3.data['data']['id'];
-                //                                             urls_images.large = response3.data['o:thumbnail_urls']['large'];
-                //                                             urls_images.me = response3.data['o:thumbnail_urls']['medium'];
-                //                                             url_media = this.buildUrlVimeoYoutube(media_type, id_video);
-                //                                         }
-                //
-                //                                         let dataVideo = {
-                //                                             id: response3.data['o:item']['o:id'],
-                //                                             title: this.getAttribEmptyOrFilled(dataResponse, 'dcterms:title'),
-                //                                             url: url_media,
-                //                                             type: media_type,
-                //                                             img_large: urls_images.large,
-                //                                             img_medium: urls_images.medium,
-                //                                         };
-                //                                         this.dataVideos.push(dataVideo);
-                //                                         console.log(this.dataVideos);
-                //                                         counter++;
-                //                                     } else {
-                //                                         if (dataResponse['bibo:contributorList'] !== undefined) {
-                //                                             let dataContribuitors = {
-                //                                                 img: this.getEmptyStringOrValue(response3.data, 'o:original_url'),
-                //                                                 description: this.getAttribEmptyOrFilled(dataResponse, 'dcterms:description'),
-                //                                                 list: this.getAttribEmptyOrFilled(dataResponse, 'bibo:contributorList').split('\n'),
-                //                                             };
-                //                                             this.dataContribuitors.push(dataContribuitors);
-                //                                         }
-                //
-                //                                     }
-                //                                 });
-                //
-                //                         })
-                //                     });
-                //
-                //             }
-                //
-                //         })
-                //
-                //
-                // })
+
+                [responseColaborares, responseVideos] = await this.$axios.all([
+                    this.$axios.get(urlColaboradores),
+                    this.$axios.get(urlVideos),
+                ]);
+
+                let colaboradores = responseColaborares.data;
+                let videos = responseVideos.data;
+                //recorremos los colaboradores
+                for (const data of colaboradores) {
+                    let url_item_set = data['o:items']['@id'];
+                    let detail = await this.$axios.get(url_item_set);
+                    let url_media = detail.data[0]['o:media'][0]['@id'];
+                    let media_item = await this.$axios.get(url_media);
+
+                    this.dataContribuitors.push({
+                        img: this.getEmptyStringOrValue(media_item.data, 'o:original_url'),
+                        description: this.getAttribEmptyOrFilled(detail.data[0], 'dcterms:description'),
+                        list: this.getAttribEmptyOrFilled(detail.data[0], 'bibo:contributorList').split('\n'),
+                    });
+                }
+
             },
             async buildCarousel() {
                 let url_item_set = this.$domainOmeka + 'api/item_sets?resource_class_label=slider&site_id=' + this.dataSite.id;
